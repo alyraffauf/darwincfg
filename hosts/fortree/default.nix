9160eacc4e854e42460b64ee6fb69768f8dcc668
Aly Raffauf <aly@raffauflabs.com>
2025-03-08 19:26:34 -0500

    fortree: add user settings

--- hosts/fortree/default.nix
+++ hosts/fortree/default.nix
@@ -86,14 +86,20 @@
     stateVersion = 6;
   };
 
-  users.users.aly.openssh.authorizedKeys = {
-    keyFiles =
-      lib.map (file: "${self.inputs.secrets}/publicKeys/${file}")
-      (lib.filter (file: lib.hasPrefix "aly_" file)
-        (builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")));
+  users.users.aly = {
+    description = "Aly Raffauf";
 
-    keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
-    ];
+    openssh.authorizedKeys = {
+      keyFiles =
+        lib.map (file: "${self.inputs.secrets}/publicKeys/${file}")
+        (lib.filter (file: lib.hasPrefix "aly_" file)
+          (builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")));
+
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
+      ];
+    };
+
+    shell = pkgs.zsh;
   };
 }
